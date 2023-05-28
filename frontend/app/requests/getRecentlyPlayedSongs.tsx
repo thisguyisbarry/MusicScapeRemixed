f4ecc0c84fc163224986ce3fca33a976efe1112d
thisguyisbarry <thisguyisbarry@gmail.com>
2023-05-28 13:18:53 +0100

    Getting song meta data for further manip

--- frontend/app/requests/getRecentlyPlayedSongs.tsx
+++ frontend/app/requests/getRecentlyPlayedSongs.tsx
@@ -1,5 +1,5 @@
 export async function getRecentlyPlayedSongs(token: string){
-    const url = "https://api.spotify.com/v1/me/player/recently-played?limit=50";
+    const url: string = "https://api.spotify.com/v1/me/player/recently-played?limit=50";
     const res = await fetch(url, {
         headers: {
             'Authorization' : 'Bearer ' + token
@@ -9,9 +9,28 @@ export async function getRecentlyPlayedSongs(token: string){
     const data = await res.json()
 
     if (res.ok){
-        console.log(data.items);
+        const songs: Array<{ track: { id: string } }>  = data.items;
+        getSongData(songs, token);
     } else {
         throw new Error("Failed to fetch recently played songs");
     }
     return token;
 }
+
+async function getSongData(songs: Array<{ track: { id: string } }>, token: string){
+    const url: string = "https://api.spotify.com/v1/audio-features/"
+
+    //console.log(songs);
+
+    songs.forEach(async song  =>{
+        let songID: string = song.track.id;
+        // console.log(songID)
+
+        let res = await fetch(url + songID, {
+            headers:{
+                'Authorization' : 'Bearer ' + token
+            }
+        });
+    })
+}
+     
