c0e4c76547280f846c46615c6808378e0c3cdc2d
thisguyisbarry <thisguyisbarry@gmail.com>
2023-05-28 18:53:49 +0100

    Data manipulation for final data object complete

--- frontend/app/components/RecentlyPlayedSongs.tsx
+++ frontend/app/components/RecentlyPlayedSongs.tsx
@@ -1,16 +1,65 @@
+import { useState, useEffect } from 'react'
+
 import { getRecentlyPlayedSongs } from "~/requests/getRecentlyPlayedSongs";
 
 interface RecentlyPlayedSongsProps {
     token: string;
 }
 
-function RecentlyPlayedSongs({ token }: RecentlyPlayedSongsProps){
-    const songs = getRecentlyPlayedSongs(token);
-    return(
-        <div>
+interface songData{
+    danceability: number,
+    key: Array<number>,
+    loudness: number,
+    valence: number,
+    tempo: number,
+    mode: number,
+    energy: number,
+    speechiness: number,
+    acousticness: number,
+    instrumentalness: number,
+    liveness: number,
+    timeDifference: number
+}
 
+
+function RecentlyPlayedSongs({ token }: RecentlyPlayedSongsProps){
+    const [songData, setSongData] = useState<songData | null>(null);
+
+    async function getSongData() {
+        try {
+          // use await to get the value of the promise
+          const data: songData = await getRecentlyPlayedSongs(token);
+          // set the state with the data
+        //   console.log(data)
+          setSongData(data);
+        } catch (error) {
+          console.error(error);
+        }
+      }
+
+    useEffect(() => {
+        getSongData();
+    }, []);
+    
+
+
+    return (
+        <div>
+          {songData ? (
+            <div>
+                <ul>
+                {Object.entries(songData).map(([key, value]) => (
+                    <li key={key}>
+                    {key}: {value}
+                    </li>
+                ))}
+                </ul>
+            </div>
+          ) : (
+            <div>Loading...</div>
+          )}
         </div>
-    )
+      );
 }
 
 export default RecentlyPlayedSongs;
--- frontend/app/requests/getRecentlyPlayedSongs.tsx
+++ frontend/app/requests/getRecentlyPlayedSongs.tsx
@@ -1,36 +1,141 @@
+interface songData{
+    danceability: number,
+    key: Array<number>,
+    loudness: number,
+    valence: number,
+    tempo: number,
+    mode: number,
+    energy: number,
+    speechiness: number,
+    acousticness: number,
+    instrumentalness: number,
+    liveness: number,
+    timeDifference: number
+}
+
+
 export async function getRecentlyPlayedSongs(token: string){
     const url: string = "https://api.spotify.com/v1/me/player/recently-played?limit=50";
+    
     const res = await fetch(url, {
         headers: {
             'Authorization' : 'Bearer ' + token
         }
     });
 
-    const data = await res.json()
+    const data = await res.json();
 
     if (res.ok){
         const songs: Array<{ track: { id: string } }>  = data.items;
-        getSongData(songs, token);
+
+        // Calculating the time difference between when first and last song were played
+        const startTime: number = Date.parse(data.items[data.items.length-1].played_at);
+        const endTime: number = Date.parse(data.items[0].played_at);
+        let timeDifference: number = (endTime - startTime) / (1000*60*60); //hours between last and most recent
+
+        if (timeDifference < 10) {
+            timeDifference = 0;
+            console.log('timeDifference' + 0)
+        } else if (timeDifference > 10 && timeDifference < 18) {
+            timeDifference = 1;
+            console.log('timeDifference' + 1)
+        } else {
+            timeDifference = 2;
+            console.log('timeDifference' + 2)
+        }
+
+        let songData: songData = await getSongData(songs, token);
+
+        songData.timeDifference = timeDifference;
+
+        console.log(songData);
+        return songData;
+
     } else {
         throw new Error("Failed to fetch recently played songs");
     }
-    return token;
+    
 }
 
-async function getSongData(songs: Array<{ track: { id: string } }>, token: string){
+async function getSongData(songs: Array<{ track: { id: string } }>, token: string): Promise<songData>{
     const url: string = "https://api.spotify.com/v1/audio-features/"
 
-    //console.log(songs);
+    let songDataObect: songData = {
+        danceability: 0,
+        key: [],
+        loudness: 0,
+        valence: 0,
+        tempo: 0,
+        mode: 0,
+        energy: 0,
+        speechiness: 0,
+        acousticness: 0,
+        instrumentalness: 0,
+        liveness: 0,
+        timeDifference: 0
+    };
 
     songs.forEach(async song  =>{
         let songID: string = song.track.id;
-        // console.log(songID)
 
         let res = await fetch(url + songID, {
             headers:{
                 'Authorization' : 'Bearer ' + token
             }
         });
+
+        const data = await res.json();
+
+        if(res.ok){
+            songDataObect.danceability      += data.danceability;
+            songDataObect.key.push(data.key);
+            songDataObect.loudness          += data.loudness;
+            songDataObect.valence           += data.valence;
+            songDataObect.tempo             += data.tempo;
+            songDataObect.mode              += data.mode;
+            songDataObect.energy            += data.energy;
+            songDataObect.speechiness       += data.speechiness;
+            songDataObect.acousticness      += data.acousticness;
+            songDataObect.instrumentalness  += data.instrumentalness;
+            songDataObect.liveness          += data.liveness;
+            songDataObect.timeDifference    += data.timeDifference;
+        } else if(res.status === 429){
+            throw new Error("Rate limited, probably duplicate song, skipping anyway. Song:" + songID);
+        } else{
+            throw new Error("Failed to fetch song meta data. Song:" + songID);
+        }
     })
+    console.log("Key " + songDataObect.key);
+    songDataObect.danceability      = songDataObect.danceability / songs.length;
+    songDataObect.key               = [mostCommonNumber(songDataObect.key)];
+    songDataObect.loudness          = songDataObect.loudness / songs.length;
+    songDataObect.valence           = songDataObect.valence / songs.length;
+    songDataObect.tempo             = songDataObect.tempo / songs.length;
+    songDataObect.mode              = songDataObect.mode / songs.length;
+    songDataObect.energy            = songDataObect.energy / songs.length;
+    songDataObect.speechiness       = songDataObect.speechiness / songs.length;
+    songDataObect.acousticness      = songDataObect.acousticness / songs.length;
+    songDataObect.instrumentalness  = songDataObect.instrumentalness / songs.length;
+    songDataObect.liveness          = songDataObect.liveness / songs.length;
+    songDataObect.timeDifference    = songDataObect.timeDifference / songs.length;
+    console.log(songDataObect);
+    return songDataObect;
 }
-     
+
+
+function mostCommonNumber(array: number[]): number {
+    if (array.length === 0){ 
+        return -1
+    } 
+    let modeMap: Record<number, number> = {}; // create an object to store the frequency of each number
+    let maxEl = array[0]; // initialize the most common number as the first element
+    let maxCount = 1; // initialize the maximum frequency as 1
+    for (let el of array) { 
+      modeMap[el] = (modeMap[el] || 0) + 1; 
+      if (modeMap[el] > maxCount) { 
+        maxEl = el; 
+        maxCount = modeMap[el]; 
+      }
+    }
+    return maxEl; // return the most common number
+  }
