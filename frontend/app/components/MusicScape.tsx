23ef81505ad7036829e483cce85e50b837884ffa
thisguyisbarry <thisguyisbarry@gmail.com>
2024-08-07 16:25:53 +0100

    More MusicScape view work

--- frontend/app/components/MusicScape.tsx
+++ frontend/app/components/MusicScape.tsx
@@ -36,7 +36,27 @@ function MusicScape({username, songData}: MusicScapeProps) {
     let sunRange: number;
     let sunPosition;
 
-    let landColours;
+    let landColours = randomColor({
+      hue: musicHues[songData.key[0]],
+      luminosity: musicLums[songData.key[0]] as "bright" | "light",  // Guaranteeing the type to TypeScipt
+      count: 5,
+      format: 'rgba',
+      alpha: 0.5
+    });;
+
+    // This is a number to determine how active a listener someone is
+    // Based on time difference between songs
+    let landNumber: number;
+
+    if(songData.timeDifference < 10){
+      landNumber = 0;
+    } else if (songData.timeDifference > 10 && songData.timeDifference < 18) {
+      landNumber = 1;
+    } else {
+      landNumber = 2;
+    }
+
+
 
 
     let valenceColour: string = musicHues[Math.round(map(songData.valence, 0.0, 1.0, 0, 7))]
@@ -79,41 +99,41 @@ function MusicScape({username, songData}: MusicScapeProps) {
     }
 
     return (
-      <div>
-        <div id="canvasDiv"></div>
-        <div className="t1">
-          <a id="username">{username}</a>
-          <a>'s musicScape</a>
-          <span className="tooltiptext">
-            <p>
-              The <b>backgound</b> of your musicScape is{" "}
-              <a id="valenceColor">{valenceColor}</a> because you've recently
-              listened to music that relates to <b id="valenceString">{songData.valenceString}</b>{" "}
-              emotions.
-            </p>
-            <p>
-              It's <b id="daynighttime">{daynighttime}</b> in your musicScape
-              because your recent music is mostly in <b id="mode">{props.mode}</b>{" "}
-              mode.
-            </p>
-            <p>
-              The mountains are <a id="energy">{props.energy}</a> jagged because
-              you've listened to <b><a id="energy1">{props.energy1}</a> energetic</b>{" "}
-              songs.
-            </p>
-            <p>
-              You've been <b><a id="listener">{props.listener}</a> listener</b>{" "}
-              during the past 24 hours, so your musicScape has{" "}
-              <a id="pairsMountains">{props.pairsMountains}</a> of mountains.
-            </p>
-            <p>
-              The <b>mountains</b> are tones of{" "}
-              <a id="keyColor">{props.keyColor}</a> because your songs are mostly
-              in the <b>key</b> <a id="keyName">{props.keyName}</a>.
-            </p>
-          </span>
-        </div>
-      </div>
+      // <div>
+      //   <div id="canvasDiv"></div>
+      //   <div className="t1">
+      //     <a id="username">{username}</a>
+      //     <a>'s musicScape</a>
+      //     <span className="tooltiptext">
+      //       <p>
+      //         The <b>backgound</b> of your musicScape is{" "}
+      //         <a id="valenceColor">{valenceColor}</a> because you've recently
+      //         listened to music that relates to <b id="valenceString">{songData.valenceString}</b>{" "}
+      //         emotions.
+      //       </p>
+      //       <p>
+      //         It's <b id="daynighttime">{daynighttime}</b> in your musicScape
+      //         because your recent music is mostly in <b id="mode">{props.mode}</b>{" "}
+      //         mode.
+      //       </p>
+      //       <p>
+      //         The mountains are <a id="energy">{props.energy}</a> jagged because
+      //         you've listened to <b><a id="energy1">{props.energy1}</a> energetic</b>{" "}
+      //         songs.
+      //       </p>
+      //       <p>
+      //         You've been <b><a id="listener">{props.listener}</a> listener</b>{" "}
+      //         during the past 24 hours, so your musicScape has{" "}
+      //         <a id="pairsMountains">{props.pairsMountains}</a> of mountains.
+      //       </p>
+      //       <p>
+      //         The <b>mountains</b> are tones of{" "}
+      //         <a id="keyColor">{props.keyColor}</a> because your songs are mostly
+      //         in the <b>key</b> <a id="keyName">{props.keyName}</a>.
+      //       </p>
+      //     </span>
+      //   </div>
+      // </div>
     );
   }
   
